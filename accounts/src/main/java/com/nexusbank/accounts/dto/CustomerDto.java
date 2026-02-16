9692cfc7342231bb4330abf0ae709e9ead5ecc14
Naveen Gajavalli <89507001+Naveengajavalli20@users.noreply.github.com>
2026-02-16 23:22:21 +0530

    spring profile configuration implementation
    * context path updated
    
    * port number updated
    
    * domain change nexus.bank -> nexusbank
    
    * configurations

---
+++ accounts/src/main/java/com/nexusbank/accounts/dto/CustomerDto.java
@@ -0,0 +1,21 @@
+package com.nexusbank.accounts.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    @NotEmpty(message = "Please provide Name")
+    @Size(min = 5, max = 25, message = "The length of the name should be between 5 and 25")
+    private String name;
+
+    @NotEmpty(message = "Please provide email address")
+    @Email(message = "please provide valid email address")
+    private String email;
+
+    @Pattern(regexp = "($|[0-9]{10})", message = "Mobile number must be 10 digits")
+    private String mobileNumber;
+
+    private AccountsDto accountsDto;
+}
---
+++ loans/src/main/java/com/nexusbank/loans/dto/ResponseDto.java
@@ -0,0 +1,14 @@
+package com.nexusbank.loans.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String statusCode;
+
+    private String statusMsg;
+
+}
